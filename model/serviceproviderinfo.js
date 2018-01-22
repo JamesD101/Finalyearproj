fcf1255b99f57e8a2f11f6ca9ffb48da4252fad4
JamesD101 <jamesodeyale01@gmail.com>
2018-01-22 18:09:14 +0100

    Update

--- model/serviceproviderinfo.js
+++ model/serviceproviderinfo.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt-nodejs');
+// const bcrypt = require('bcrypt-nodejs');
 
 const serviceProviderSchema = new Schema ({
       description : { type: String, required: true, unique: true },
       profilepic : { type: String }
 
 });
+
+module.exports = mongoose.exports('spinfo', serviceProviderSchema);
